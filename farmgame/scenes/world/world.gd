5a120a3289e6d11cfd404033d0f8f27b2fb264ab
unjerry <2351688512@qq.com>
2026-05-16 13:28:06 +0800

    stage

--- farmgame/scenes/world/world.gd
+++ farmgame/scenes/world/world.gd
@@ -1,8 +1,14 @@
 extends Node2D
 @export var farm_tile_scene: PackedScene
+var grid_width := 3
+var grid_height := 4
+var tile_stride := 64
+var farm_start_position := Vector2(100, 200)
 func _ready() -> void:
-	create_one_farm_tile()
-func create_one_farm_tile() -> void:
-	var farm_tile := farm_tile_scene.instantiate()
-	$FarmTiles.add_child(farm_tile)
-	farm_tile.position = Vector2(300, 300)
+	create_farm_tile()
+func create_farm_tile() -> void:
+	for y in grid_height:
+		for x in grid_width:
+			var farm_tile := farm_tile_scene.instantiate()
+			$FarmTiles.add_child(farm_tile)
+			farm_tile.position = farm_start_position + Vector2(x * tile_stride, y * tile_stride)
